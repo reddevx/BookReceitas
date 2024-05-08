1ac8c859886e8ba1c68ad34e26a43fedb1a12388
reddevx <desenvolvedor.juan.business@gmail.com>
2024-05-07 22:21:00 -0500

    chore: add class User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_07_135831) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_08_031207) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_07_135831) do
     t.text "description"
     t.text "ingredients"
     t.text "instructions"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "password_digest", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
